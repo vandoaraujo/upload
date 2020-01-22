dff6e17300eb4865483bdcddc8498a3b5227338f
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2020-01-22 10:47:09 -0200

    adição do projeto POI

---
+++ src/main/java/com/spring/upload/controller/ExcelLeitura.java
@@ -0,0 +1,57 @@
+package com.spring.upload.controller;
+
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Created by rajeevkumarsingh on 18/12/17.
+ */
+
+public class ExcelLeitura {
+    public static final String SAMPLE_XLSX_FILE_PATH = "./Programação2.xlsx";
+
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+
+        // Creating a Workbook from an Excel file (.xls or .xlsx)
+        Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
+
+        // Retrieving the number of sheets in the Workbook
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        // 3. Or you can use a Java 8 forEach wih lambda
+        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
+        workbook.forEach(sheet -> {
+            System.out.println("=> " + sheet.getSheetName());
+        });
+
+        // Getting the Sheet at index zero
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+        
+        //1/16/20
+        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                String cellValue = dataFormatter.formatCellValue(cell);
+                if(cellValue.contains("16-jan-2020"))
+                	System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+
+        // Closing the workbook
+        workbook.close();
+    }
+}
