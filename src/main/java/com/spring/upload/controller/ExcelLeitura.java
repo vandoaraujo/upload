816b314568d2c90187caefd0cbcbf938f55eb35c
Vando Araujo <tecladista.vando@gmail.com>
2020-01-22 22:21:46 -0300

    algoritmo
    algoritmo de leitura de planilha

--- src/main/java/com/spring/upload/controller/ExcelLeitura.java
+++ src/main/java/com/spring/upload/controller/ExcelLeitura.java
@@ -1,12 +1,15 @@
 package com.spring.upload.controller;
 
 
+import ch.qos.logback.core.CoreConstants;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by rajeevkumarsingh on 18/12/17.
@@ -15,13 +18,12 @@ import java.util.Iterator;
 public class ExcelLeitura {
     public static final String SAMPLE_XLSX_FILE_PATH = "./Programação2.xlsx";
 
+    static List<String> colunas = null;
+
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
         // Creating a Workbook from an Excel file (.xls or .xlsx)
         Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
-
-        // Retrieving the number of sheets in the Workbook
-        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
 
         // 3. Or you can use a Java 8 forEach wih lambda
         System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
@@ -31,27 +33,61 @@ public class ExcelLeitura {
 
         // Getting the Sheet at index zero
         Sheet sheet = workbook.getSheetAt(0);
-
-        // Create a DataFormatter to format and get each cell's value as String
-        DataFormatter dataFormatter = new DataFormatter();
-        
-        //1/16/20
-        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+//        Map<String, RowLight> map = new HashMap();
         Iterator<Row> rowIterator = sheet.rowIterator();
+        boolean primeiraLinha = true;
+
         while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
             Iterator<Cell> cellIterator = row.cellIterator();
-
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                String cellValue = dataFormatter.formatCellValue(cell);
-                if(cellValue.contains("16-jan-2020"))
-                	System.out.print(cellValue + "\t");
+            if(primeiraLinha){
+                 colunas = builderHeader(row);
+                colunas.forEach(s -> {
+                    System.out.println(s);
+                });
+                primeiraLinha = false;
             }
-            System.out.println();
+            if(isRowSearched("1/16/20", row)){
+                buildRowSearched(row);
+            }
         }
 
         // Closing the workbook
         workbook.close();
+    }
+
+    private static List<String> builderHeader(Row row) {
+        List<String> headerColunas = new ArrayList();
+        Iterator<Cell> cellIterator = row.cellIterator();
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            String cellValue = new DataFormatter().formatCellValue(cell);
+            headerColunas.add(cellValue);
+        }
+        return headerColunas;
+    }
+
+    private static boolean isRowSearched(String data, Row row ) {
+        Iterator<Cell> cellIterator = row.cellIterator();
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            String cellValue = new DataFormatter().formatCellValue(cell);
+            if(!cellValue.equals("") && cellValue.equals(data)) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private static void buildRowSearched( Row row ) {
+        Iterator<Cell> cellIterator = row.cellIterator();
+
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            String cellValue = new DataFormatter().formatCellValue(cell);
+            System.out.print(cellValue + "\t");
+        }
+        System.out.println();
     }
 }
