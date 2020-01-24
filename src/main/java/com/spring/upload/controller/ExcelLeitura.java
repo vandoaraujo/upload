f35958691f63900dc1351628f78bea229c182382
Vando Araujo <tecladista.vando@gmail.com>
2020-01-24 00:04:10 -0300

    formatacao
    formatacao da saida

--- src/main/java/com/spring/upload/controller/ExcelLeitura.java
+++ src/main/java/com/spring/upload/controller/ExcelLeitura.java
@@ -24,36 +24,63 @@ public class ExcelLeitura {
 
         // Creating a Workbook from an Excel file (.xls or .xlsx)
         Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
-
-        // 3. Or you can use a Java 8 forEach wih lambda
-        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
-        workbook.forEach(sheet -> {
-            System.out.println("=> " + sheet.getSheetName());
-        });
-
+        String data = "1/16/20";
         // Getting the Sheet at index zero
         Sheet sheet = workbook.getSheetAt(0);
 //        Map<String, RowLight> map = new HashMap();
         Iterator<Row> rowIterator = sheet.rowIterator();
         boolean primeiraLinha = true;
+        List<Row> foundRows = new ArrayList<>();
 
         while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
             Iterator<Cell> cellIterator = row.cellIterator();
             if(primeiraLinha){
-                 colunas = builderHeader(row);
-                colunas.forEach(s -> {
-                    System.out.println(s);
-                });
+                colunas = builderHeader(row);
+//                colunas.forEach(s -> {
+////                    System.out.println(s);
+//                });
                 primeiraLinha = false;
             }
-            if(isRowSearched("1/16/20", row)){
-                buildRowSearched(row);
+            if(isRowSearched(data, row)){
+                foundRows.add(row);
+              //  buildRowSearched(row);
             }
+
+            formataSaida(data, colunas, foundRows);
         }
 
         // Closing the workbook
         workbook.close();
+    }
+
+    /**
+     * *MANUTENÇÃO E OPERAÇÃO DTR-SE*
+     * *RESUMO DIÁRIO DO ATENDIMENTO - 16/01/2020*
+     * EQUIPE 07:00 X 16:00
+     *
+     * SETD PER RELÉ Digital Modernização e testes do sistema de controle e proteção.
+     * Equipe: NEANDER E GATTI
+     * Viatura: 2047
+     * Horário de saída: 08:30
+     * Status:
+     * Justificativa: DDS
+     * @param colunas
+     * @param foundRows
+     */
+    private static void formataSaida(String data, List<String> colunas, List<Row> foundRows) {
+        System.out.println("MANUTENÇÃO E OPERAÇÃO DTR-SE");
+        System.out.print("RESUMO DIÁRIO DO ATENDIMENTO - ");
+        System.out.println(data);
+        System.out.println("EQUIPE 07:00 X 16:00");
+
+        for (int i=0 ; i< colunas.size(); i++){
+            if(colunas.get(i).equals("Subestação DTR-SE")){
+                System.out.println("SETD");
+                System.out.print(foundRows.get(i));
+            }
+    //        Subestação DTR-SE	Equipamento
+        }
     }
 
     private static List<String> builderHeader(Row row) {
@@ -82,7 +109,6 @@ public class ExcelLeitura {
 
     private static void buildRowSearched( Row row ) {
         Iterator<Cell> cellIterator = row.cellIterator();
-
         while (cellIterator.hasNext()) {
             Cell cell = cellIterator.next();
             String cellValue = new DataFormatter().formatCellValue(cell);
