bfa77a4aa07f63b8b2ab563a847faf7c03a1f9e1
Vando Araujo <tecladista.vando@gmail.com>
2020-01-28 00:58:29 -0300

    testes

--- src/main/java/com/spring/upload/controller/ExcelLeitura.java
+++ src/main/java/com/spring/upload/controller/ExcelLeitura.java
@@ -25,11 +25,13 @@ public class ExcelLeitura {
 
 	static List<String> colunas = null;
 	static List<Map> valorContidoEmUmaLinha = new ArrayList<>();
+	static List<Row> linhasCanceladas = new ArrayList();
 
 	public static void main(String[] args) throws IOException, InvalidFormatException {
 
 		Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
 		String data = "1/16/20";
+		String hora = "07:00 X 16:00";
 		Sheet sheet = workbook.getSheetAt(0);
 		Iterator<Row> rowIterator = sheet.rowIterator();
 		boolean primeiraLinha = true;
@@ -40,7 +42,7 @@ public class ExcelLeitura {
 				colunas = builderHeader(linha);
 				primeiraLinha = false;
 			}
-			if (isLinhaEncontrada(data, linha)) {
+			if (isDataEncontrada(data, linha) && isHoraEncontrada(hora, linha) && isRealizado(linha)) {
 				// Coluna -> Valor
 				Map<String, String> headerComValorCadaCelula = new HashMap<>();
 				Iterator<Cell> cellIterator = linha.cellIterator();
@@ -73,7 +75,6 @@ public class ExcelLeitura {
 	 * Status: Justificativa: DDS
 	 * 
 	 * @param colunas
-	 * @param foundRows
 	 */
 	private static void formataSaida(String data, List<String> colunas) {
 		System.out.println("MANUTENÇÃO E OPERAÇÃO DTR-SE");
@@ -81,28 +82,68 @@ public class ExcelLeitura {
 		System.out.println(data);
 		System.out.println("EQUIPE 07:00 X 16:00");
 
+		for (Map row : valorContidoEmUmaLinha) {
+			System.out.print("SETD ");
+			imprimeValores(colunas, "Subestação DTR-SE", row);
+			imprimeValores(colunas, "Equipamento", row);
+			imprimeValores(colunas, "Tipo de Equipamento", row);
+			imprimeValores(colunas, "Causa/Serviço", row);
+			System.out.println();
+			System.out.print("Equipe: ");
+			String valor = buscaColaborador(colunas, "Colaborador 1 - DTR-SE", row);
+			System.out.print(valor);
+			String colaborador2 = buscaColaborador(colunas, "Colaborador 2 - DTR-SE", row);
+			if(colaborador2 != "") {
+				System.out.print(", " + colaborador2);
+			}
+			String colaborador3 = buscaColaborador(colunas, "Colaborador 3 - DTR-SE", row);
+			if(colaborador3 != "") {
+				System.out.print(", " + colaborador3);
+			}
+			String colaborador4 = buscaColaborador(colunas, "Colaborador 4 - DTR-SE", row);
+			if(colaborador4 != "") {
+				System.out.print(", " + colaborador4);
+			}
+			String colaborador5 = buscaColaborador(colunas, "Observação Pós Programação", row);
+			if(colaborador5 != "") {
+				System.out.print(", " + colaborador5);
+			}
+			System.out.println();
+			System.out.println("Viatura: ");
+			System.out.println("Horário de Saída: ");
+			System.out.println("Status: ");
+			System.out.println("Justificativa: ");
+			System.out.println();
+		}
+	}
+
+	private static void imprimeValores(List<String> colunas, String coluna, Map row ) {
 		for (int i = 0; i < colunas.size(); i++) {
-			if (colunas.get(i).equals("Subestação DTR-SE")) {
-				System.out.println("SETD");
-				boolean achou = false;
-				for (Map row : valorContidoEmUmaLinha) {
+			if (colunas.get(i).equals(coluna)) {
 					Set<String> valores = row.keySet();
 					for (String key : valores) {
-						if(key.equals("Subestação DTR-SE")) {
+						if(key.equals(coluna)) {
 							String value = (String) row.get(key);
-							System.out.printf(value);
-							achou = true;
+							System.out.print(value + " ");
 							break;
 						}
-						
 					}
-					if(achou) {
-						break;
+			}
+		}
+	}
+
+	private static String buscaColaborador(List<String> colunas, String coluna, Map row ) {
+		for (int i = 0; i < colunas.size(); i++) {
+			if (colunas.get(i).equals(coluna)) {
+				Set<String> valores = row.keySet();
+				for (String key : valores) {
+					if(key.equals(coluna)) {
+						return (String) row.get(key);
 					}
 				}
 			}
-			// Subestação DTR-SE Equipamento
 		}
+		return "";
 	}
 
 	private static List<String> builderHeader(Row row) {
@@ -116,7 +157,7 @@ public class ExcelLeitura {
 		return headerColunas;
 	}
 
-	private static boolean isLinhaEncontrada(String data, Row row) {
+	private static boolean isDataEncontrada(String data, Row row) {
 		Iterator<Cell> cellIterator = row.cellIterator();
 		while (cellIterator.hasNext()) {
 			Cell cell = cellIterator.next();
@@ -127,6 +168,37 @@ public class ExcelLeitura {
 			return false;
 		}
 		return false;
+	}
+
+	private static boolean isHoraEncontrada(String hora, Row row) {
+		Iterator<Cell> cellIterator = row.cellIterator();
+		boolean achou = false;
+		while (cellIterator.hasNext()) {
+			Cell cell = cellIterator.next();
+			String cellValue = new DataFormatter().formatCellValue(cell);
+			if (!cellValue.equals("") && (cellValue.equals(hora))) {
+				achou = true;
+				break;
+			}
+			achou = false;
+		}
+		return achou;
+	}
+
+	private static boolean isRealizado(Row row) {
+		Iterator<Cell> cellIterator = row.cellIterator();
+		boolean realizado = false;
+		while (cellIterator.hasNext()) {
+			Cell cell = cellIterator.next();
+			String cellValue = new DataFormatter().formatCellValue(cell);
+			if (!cellValue.equals("") && (cellValue.startsWith("Cancelado"))) {
+				linhasCanceladas.add(row);
+				realizado = false;
+				break;
+			}
+			realizado = true;
+		}
+		return realizado;
 	}
 
 	private static void buildRowSearched(Row row) {
